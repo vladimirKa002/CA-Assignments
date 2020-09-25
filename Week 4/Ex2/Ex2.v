eecdbfd8619fdcd34095db4b3b0a18f867e9b2ad
vladimirKa002 <kvvgames18@gmail.com>
2020-09-25 07:03:22 +0300

    Lab Assignments Week4

--- Week 4/Ex2/Ex2.v
+++ Week 4/Ex2/Ex2.v
@@ -8,11 +8,11 @@ module Ex2(
 	output		     [7:0]		HEX5
 );
 
-assign HEX0 = KEY[0] ? 8'b11111100 : 8'b00000000;
-assign HEX1 = KEY[0] ? 8'b01100000 : 8'b00000000;
-assign HEX2 = KEY[0] ? 8'b11111100 : 8'b00000000;
-assign HEX3 = KEY[0] ? 8'b11110110 : 8'b00000000;
-assign HEX4 = KEY[0] ? 8'b11111100 : 8'b00000000;
-assign HEX5 = KEY[0] ? 8'b11011010 : 8'b00000000;
+assign HEX5 = KEY[0] ? 8'b11111100 : 8'b00000000;
+assign HEX4 = KEY[0] ? 8'b01100000 : 8'b00000000;
+assign HEX3 = KEY[0] ? 8'b11111100 : 8'b00000000;
+assign HEX2 = KEY[0] ? 8'b11110110 : 8'b00000000;
+assign HEX1 = KEY[0] ? 8'b11111100 : 8'b00000000;
+assign HEX0 = KEY[0] ? 8'b11011010 : 8'b00000000;
 
 endmodule 
